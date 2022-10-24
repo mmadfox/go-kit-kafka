75571450222da25bde06705841f9a805388d35cd
mmadfox <sergey.liskonog@gmail.com>
2022-10-24 23:38:12 +0300

    add tracing

---
+++ opentracing.go
@@ -0,0 +1,26 @@
+package gokitkafka
+
+import (
+	"github.com/go-kit/kit/endpoint"
+	kitopentracing "github.com/go-kit/kit/tracing/opentracing"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+// TraceConsumer returns an endpoint.Middleware that wraps the `next` endpoint.Endpoint in an
+// OpenTracing Span called `operationName` with consumer span.kind tag.
+func TraceConsumer(tracer opentracing.Tracer, operationName string, opts ...kitopentracing.EndpointOption) endpoint.Middleware {
+	opts = append(opts, kitopentracing.WithTags(map[string]interface{}{
+		ext.SpanKindConsumer.Key: ext.SpanKindConsumer.Value,
+	}))
+	return kitopentracing.TraceEndpoint(tracer, operationName, opts...)
+}
+
+// TraceProducer returns an endpoint.Middleware that wraps the `next` endpoint.Endpoint in an
+// OpenTracing Span called `operationName` with producer span.kind tag.
+func TraceProducer(tracer opentracing.Tracer, operationName string, opts ...kitopentracing.EndpointOption) endpoint.Middleware {
+	opts = append(opts, kitopentracing.WithTags(map[string]interface{}{
+		ext.SpanKindProducer.Key: ext.SpanKindProducer.Value,
+	}))
+	return kitopentracing.TraceEndpoint(tracer, operationName, opts...)
+}
