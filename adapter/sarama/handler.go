53b245d35140556759bd780d638a07b632c6a7ad
mmadfox <sergey.liskonog@gmail.com>
2022-10-31 21:20:49 +0300

    refactor: add handlers funcs

--- adapter/sarama/handler.go
+++ adapter/sarama/handler.go
@@ -10,12 +10,18 @@ import (
 
 type HandlerOption func(*Handler)
 
+type HookFunc func(sarama.ConsumerGroupSession) error
+
+type HandlerBeforeFunc func(context.Context, *sarama.ConsumerMessage) error
+type HandlerAfterFunc func(context.Context, sarama.ConsumerGroupSession, *sarama.ConsumerMessage)
+
 type Handler struct {
 	handler      gokitkafka.Handler
 	errorHandler transport.ErrorHandler
-	onSetup      []func(sarama.ConsumerGroupSession) error
-	onCleanup    []func(sarama.ConsumerGroupSession) error
-	before       []func(ctx context.Context, msg *sarama.ConsumerMessage) error
+	onSetup      []HookFunc
+	onCleanup    []HookFunc
+	before       []HandlerBeforeFunc
+	after        HandlerAfterFunc
 }
 
 func NewHandler(h gokitkafka.Handler, opts ...HandlerOption) (*Handler, error) {
@@ -32,21 +38,27 @@ func NewHandler(h gokitkafka.Handler, opts ...HandlerOption) (*Handler, error) {
 	return handler, nil
 }
 
-func HandlerOnSetup(fn ...func(sarama.ConsumerGroupSession) error) HandlerOption {
+func HandlerOnSetup(fn ...HookFunc) HandlerOption {
 	return func(h *Handler) {
 		h.onSetup = append(h.onSetup, fn...)
 	}
 }
 
-func HandlerOnCleanup(fn ...func(sarama.ConsumerGroupSession) error) HandlerOption {
+func HandlerOnCleanup(fn ...HookFunc) HandlerOption {
 	return func(h *Handler) {
 		h.onCleanup = append(h.onCleanup, fn...)
 	}
 }
 
-func HandlerBefore(before ...func(ctx context.Context, msg *sarama.ConsumerMessage) error) HandlerOption {
+func HandlerBefore(before ...HandlerBeforeFunc) HandlerOption {
 	return func(h *Handler) {
 		h.before = append(h.before, before...)
+	}
+}
+
+func HandlerAfter(after HandlerAfterFunc) HandlerOption {
+	return func(h *Handler) {
+		h.after = after
 	}
 }
 
@@ -73,18 +85,30 @@ func (h *Handler) ConsumeClaim(
 	claim sarama.ConsumerGroupClaim,
 ) error {
 	ctx := session.Context()
-	for msg := range claim.Messages() {
-		for i := 0; i < len(h.before); i++ {
-			if err := h.before[i](ctx, msg); err != nil {
+
+	for {
+		select {
+		case msg := <-claim.Messages():
+			for i := 0; i < len(h.before); i++ {
+				if err := h.before[i](ctx, msg); err != nil {
+					h.errorHandler.Handle(ctx, err)
+					continue
+				}
+			}
+
+			if err := h.handler.HandleMessage(ctx, convertSaramaToKafka(msg)); err != nil {
 				h.errorHandler.Handle(ctx, err)
 				continue
 			}
+
+			if h.after != nil {
+				h.after(ctx, session, msg)
+			} else {
+				session.MarkMessage(msg, "")
+			}
+
+		case <-ctx.Done():
+			return nil
 		}
-		if err := h.handler.HandleMessage(ctx, convertSaramaToKafka(msg)); err != nil {
-			h.errorHandler.Handle(ctx, err)
-			continue
-		}
-		session.MarkMessage(msg, "")
 	}
-	return nil
 }
